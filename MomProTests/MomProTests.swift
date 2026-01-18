6c6094a2fad1e5cbfcf04f2fe431038b3f75f75f
ilCatta <andreaagostino.cataldo@gmail.com>
2026-01-18 19:03:30 +0100

    Initial Commit

---
+++ MomProTests/MomProTests.swift
@@ -0,0 +1,17 @@
+//
+//  MomProTests.swift
+//  MomProTests
+//
+//  Created by Andrea Cataldo on 18/01/26.
+//
+
+import Testing
+@testable import MomPro
+
+struct MomProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
